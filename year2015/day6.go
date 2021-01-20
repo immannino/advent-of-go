d916063e5b166d1279ba4eea47aa24f0f81fa1ac
Tony <tonym@sb401k.com>
2021-01-19 19:34:02 -0800

    Pushing changes for day6

--- year2015/day6.go
+++ year2015/day6.go
@@ -7,7 +7,8 @@ import (
 	"strings"
 )
 
-type coords struct {
+// Coords is a coords struct
+type Coords struct {
 	aStart, aEnd int
 	bStart, bEnd int
 }
@@ -15,30 +16,48 @@ type coords struct {
 // Day6 -- Probably a Fire Hazard
 func Day6() {
 	data := utils.GetData("data/2015/day6.txt")
-
-	list := strings.Split(data, "\n")
-
-	coords := make([]bool, 1000000)
+	list := strings.Split(data, "\r\n")
+	coords := make([]uint8, 1000000)
+	rowSize := 1000
 
 	for _, c := range list {
-		vals := parseIns(c)
+		vals, action := parseIns(c)
 
-		toggleLights(&coords, vals)
+		toggleLights(&coords, vals, action, rowSize)
+		// fmt.Println(getLightCount(coords), action, vals)
 	}
 
-	fmt.Printf("Day  6: { 1: %d, 2: %d }\n", 0, len(list))
+	litCount := getLightCount(coords)
+
+	fmt.Printf("Day  6: { 1: %d, 2: %d }\n", litCount, len(list))
 }
 
-func parseIns(ins string) *coords {
+func getLightCount(lights []uint8) int {
+	litCount := 0
+
+	for _, c := range lights {
+		if c == 1 {
+			litCount++
+		}
+	}
+
+	return litCount
+}
+
+func parseIns(ins string) (*Coords, string) {
 	var data string
 	var vals []string
+	var action string
 
 	if strings.Contains(ins, "turn on") {
 		data = strings.TrimPrefix(ins, "turn on ")
+		action = "TURNON"
 	} else if strings.Contains(ins, "turn off") {
-		data = strings.TrimPrefix(ins, "turn on ")
+		data = strings.TrimPrefix(ins, "turn off ")
+		action = "TURNOFF"
 	} else {
-		data = strings.TrimPrefix(ins, "turn on ")
+		data = strings.TrimPrefix(ins, "toggle ")
+		action = "TOGGLE"
 	}
 
 	vals = strings.Split(data, " through ")
@@ -50,19 +69,46 @@ func parseIns(ins string) *coords {
 	bStart, _ := strconv.Atoi(b[0])
 	bEnd, _ := strconv.Atoi(b[1])
 
-	val := coords{
+	val := Coords{
 		aStart,
 		aEnd,
 		bStart,
 		bEnd,
 	}
 
-	return &val
+	return &val, action
 }
 
-func toggleLights(lights *[]bool, vals coords) {
+func toggleLights(lights *[]uint8, vals *Coords, action string, rowSize int) {
 	// To calculate where to Start & end:
 	// (Y * rowSize) + X
-	//
-	//
+
+	start := (vals.aEnd * rowSize) + vals.aStart
+	end := (vals.bEnd * rowSize) + vals.bStart
+
+	// fmt.Printf("%s - s: {%d, %d} e: {%d, %d} Count: %d \n", action, vals.aStart, vals.aEnd, vals.bStart, vals.bEnd, (end - start))
+
+	// Potentially need end + 1
+	for i := start; i <= end; i++ {
+		switch action {
+		case "TURNON":
+			(*lights)[i] = 1
+		case "TURNOFF":
+			(*lights)[i] = 0
+		case "TOGGLE":
+			if (*lights)[i] == 1 {
+				(*lights)[i] = 0
+			} else {
+				(*lights)[i] = 1
+			}
+		}
+	}
+}
+
+func fancyPrint(lights *[]uint8, rowSize int) {
+	for i := 0; i < rowSize; i++ {
+		row := (*lights)[i:((rowSize * i) + rowSize)]
+		rowString := strings.Trim(strings.Join(strings.Split(fmt.Sprint(row), " "), ""), "[]")
+		fmt.Println(rowString)
+	}
 }
---
+++ year2015/day6_test.go
@@ -0,0 +1,78 @@
+package year2015
+
+import (
+	"fmt"
+	"testing"
+)
+
+// TestOvo testing to help myself
+func TestToggleLights(t *testing.T) {
+	lights := make([]uint8, 1000000)
+	lights1 := make([]uint8, 1000000)
+	lights2 := make([]uint8, 1000000)
+
+	var tests = []struct {
+		lights  *[]uint8
+		vals    *Coords
+		action  string
+		rowSize int
+		count   int
+	}{
+		{
+			lights: &lights,
+			vals: &Coords{
+				aStart: 0,
+				aEnd:   0,
+				bStart: 999,
+				bEnd:   999,
+			},
+			rowSize: 1000,
+			action:  "TURNON",
+			count:   1000000,
+		},
+		{
+			lights: &lights1,
+			vals: &Coords{
+				aStart: 0,
+				aEnd:   0,
+				bStart: 999,
+				bEnd:   0,
+			},
+			rowSize: 1000,
+			action:  "TOGGLE",
+			count:   1000,
+		},
+		{
+			lights: &lights2,
+			vals: &Coords{
+				aStart: 499,
+				aEnd:   499,
+				bStart: 500,
+				bEnd:   500,
+			},
+			rowSize: 1000,
+			action:  "TURNOFF",
+			count:   0,
+		},
+	}
+
+	for _, tt := range tests {
+		testname := fmt.Sprintf("%s", tt.action)
+
+		t.Run(testname, func(t *testing.T) {
+			toggleLights(tt.lights, tt.vals, tt.action, tt.rowSize)
+
+			actual := 0
+
+			for _, c := range *tt.lights {
+				if c == 1 {
+					actual++
+				}
+			}
+
+			if actual != tt.count {
+				t.Errorf("got %d, expected %d", actual, tt.count)
+			}
+		})
+	}
+}
