f4c26534150208a9546048cc9185d5746b6f0e8e
Tones <tonymannino@me.com>
2021-12-06 22:47:57 -0800

    Day 7 in da books

--- main.go
+++ main.go
@@ -19,6 +19,7 @@ func main() {
 	// year2015.Day6()
 	// // year2015.Day7()
 	// year2015.Day8()
+	// fmt.Printf("\n\nYear 2015 Complete. Elapsed time: %s\n", elapsed)
 
 	year2021.Day1()
 	year2021.Day2()
@@ -26,8 +27,9 @@ func main() {
 	year2021.Day4()
 	year2021.Day5()
 	year2021.Day6()
+	year2021.Day7()
 
 	elapsed := time.Since(start)
 
-	fmt.Printf("\n\nYear 2015 Complete. Elapsed time: %s\n", elapsed)
+	fmt.Printf("\n\nYear 2021 Complete. Elapsed time: %s\n", elapsed)
 }
